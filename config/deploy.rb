a47974b3f9a57ce9a36c535e0e05e8605ce07cdc
borden jardine <bordenjardine@gmail.com>
2014-11-25 18:38:54 -0500

    test controller

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,12 +40,12 @@ set :user, 'borden'
 namespace :deploy do
 
   desc 'Restart application'
-#  task :restart do
-#    on roles(:app), in: :sequence, wait: 5 do
-#      # Your restart mechanism here, for example:
-#      execute :touch, release_path.join('tmp/restart.txt')
-#    end
-#  end
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
 
   after :publishing, :restart
   after :finishing, :cleanup
